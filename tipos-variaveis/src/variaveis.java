54d42721f14b941a03fb40223fba6fede200f47b
Luciano Cleistenes <lucianocleistenes@gmail.com>
2023-02-15 20:49:23 +0000

    modified file to apply arguments

--- tipos-variaveis/src/variaveis.java
+++ tipos-variaveis/src/variaveis.java
@@ -1,7 +1,13 @@
 public class variaveis {
-    public static void main(String[] args) throws Exception {
-        final int NUMERO = 5;
-        numero = 10;
-        System.out.print(numero);
+    public static void main(String[] args) {
+        //os argumentos começam com índice 0
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args [2]);
+        double altura = Double.valueOf(args [3]);
+
+        System.out.println("Olá meu nome é " + nome + sobrenome);
+        System.out.println("Tenho " + idade + "anos");
+        System.out.println("MInha altura é" + altura + "cm");
     }
 }
